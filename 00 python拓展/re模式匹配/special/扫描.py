1e09c2ad1835c6b60bc440fcbc926cea67db7089
i3-10105F <123@qq.com>
2021-10-23 18:49:45 +0800

    commit in 1023

---
+++ 00 python拓展/re模式匹配/special/扫描.py
@@ -0,0 +1,13 @@
+import re
+
+# match和search的区别：match从头开始扫描匹配，search扫描整个字符串
+# match
+pattern = "^[0-9]*$"  # 空或者只包含数字
+test_str = ""
+ret = re.match(pattern, test_str, flags=0)
+print(ret)
+# print(dir(ret))
+
+# search
+ret = re.match(pattern, test_str, flags=0)
+print(ret)
