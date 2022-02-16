9cbbb67fb194d17acf7599350f980a31e6f1101e
newyQs <73620758+newyQs@users.noreply.github.com>
2022-02-16 23:57:35 +0800

    'test'

--- 01 基础入门/01 基本语法/07 流程语句.py
+++ 01 基础入门/01 基本语法/07 流程语句.py
@@ -1,17 +1,17 @@
 # 1.条件判断
-'''
+"""
 （1）if ...
 （2）if ...else ...
 （3）if ...elif ...else ...
-'''
+"""
 
 # 2.循环语句
-'''
+"""
 （1）for...in
 （2）while... 
 （3）for ...in ...else
 （4）while ...else ...
-'''
+"""
 for i in [1, 2, 3, 4]:
     if i == 2:
         print(i)
@@ -20,7 +20,7 @@ else:
     print('找不到')
 
 # 3.控制语句
-'''
+"""
  continue  跳出本次循环
  break 终止循环
-'''
+"""
