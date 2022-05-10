52452e29acd47d9535475d038b6fded9f56b246b
newyQs <73620758+newyQs@users.noreply.github.com>
2022-05-11 00:15:23 +0800

    'fis'

---
+++ 02 拓展提升/执行cmd/sub.py
@@ -0,0 +1,47 @@
+"""
+os模块中常见方法(Linux命令)
+
+os.remove()：删除文件
+
+os.rename()：重命名文件
+
+os.walk()：生成目录树下的所有文件名
+
+os.chdir()：改变目录
+
+os.mkdir/makedirs：创建目录/多层目录
+
+os.rmdir/removedirs:删除目录/多层目录
+
+os.listdir()：列出指定目录的文件
+
+os.getcwd()：取得当前工作目录
+
+os.chmod()：改变目录权限
+
+os.path.basename()：去掉目录路径，返回文件名
+
+os.path.dirname()：去掉文件名，返回目录路径
+
+os.path.join()：将分离的各部分组合成一个路径名
+
+os.path.getsize()：返回文件大小
+
+os.path.exists()：是否存在
+
+os.path.isabs()：是否为绝对路径
+
+os.path.isdir()：是否为目录
+
+os.path.isfile()：是否为文件
+"""
+import subprocess
+import os
+
+# file_path = "./sub.py"
+#
+# cmd = f"file {file_path}"
+# status, output = subprocess.run(cmd)
+# print(status, output)
+
+print(os.getcwd())
